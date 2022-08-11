6298249cd7acad4f21a89ca29180b8f045646f2b
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-08-11 12:06:35 -0700

    get request is working properly, changed change e function in order to type

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -28,7 +28,7 @@ return(
         <main>
             <div className="actions--bar">
                 <div className="wrap">
-                    <Link className="button" to="/courses/:id/update">Update Course</Link>
+                    <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
                     <Link className="button" to="/" onClick={ deleteCourse }>Delete Course</Link> 
                     <Link className="button button-secondary" to="/">Return to List</Link>
                 </div>
--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -17,34 +17,60 @@ export default function UpdateCourse({context}) {
     const [description, setDescription] = useState('')
     const [estimatedTime, setTime] = useState ('')
     const [materialsNeeded, setmaterialsNeeded] = useState('')
-    const [errors, errorsNeeded] = useState ('');   
+    const [errors, setErrors] = useState ('');   
 
-    //how to use a put request with fetch => https://jasonwatmore.com/post/2021/09/20/fetch-http-put-request-examples 
-    function handleSubmit () {
-        const UpdateCourse= {
-            title,
-            description,
-            estimatedTime,
-            materialsNeeded,
-            userId: authUser.id
-        }
-    const requestOptions = {
-        method: 'PUT',
-        headers: { 
-            'Authorization': 'Basic ' + Buffer.from(`${authUser.emailAddress}:${authUser.password}`).toString("base64"),
-            "Content-Type": "application/json" ,    
-        },
-        body: JSON.stringify({ UpdateCourse })
+    const UpdateCourse= {
+        title,
+        description,
+        estimatedTime,
+        materialsNeeded,
+        userId: authUser.id
     };
-        fetch(`http://localhost:5000/api/courses/${id}`, requestOptions)
-        .then(response => response.json())
-        .then(value => UpdateCourse({
-            setTitle: value.title,
-            setDescription: value.description,
-            setTime: value.estimatedTime,
-            setmaterialsNeeded: value.materialsNeeded
-    }))
-    };
+    // setup course state with the properties that the course object has
+        const [course, setCourse] = useState({
+            id: '',
+            title: '',
+            description: '',
+            estimatedTime: '',
+            materialsNeeded: '',
+            user: {
+                id: '',
+                firstName: '',
+                lastName: '',
+                emailAddress: ''
+            }        
+          });
+          useEffect(() => {
+            function fetchCourses() {
+            fetch(`http://localhost:5000/api/courses/${id}`) //course variable is empty here 
+            .then ((response) => response.json())
+            .then ((data) => setCourse(data.courses))
+            .catch(err => console.log('Oh noes!', err))
+            }
+                fetchCourses();
+                console.log("fetch was successful")
+            }, []);
+            
+
+        function handleSubmit () {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 
+                    'Authorization': 'Basic ' + Buffer.from(`${authUser.emailAddress}:${authUser.password}`).toString("base64"),
+                    "Content-Type": "application/json" ,    
+                },
+                body: JSON.stringify({ UpdateCourse })}
+
+            fetch(`http://localhost:5000/api/courses/${id}`, requestOptions) //how to use a put request with fetch => https://jasonwatmore.com/post/2021/09/20/fetch-http-put-request-examples 
+    
+            // .then(response => response.json())
+            // .then(value => UpdateCourse({
+            //     setTitle: value.title,
+            //     setDescription: value.description,
+            //     setTime: value.estimatedTime,
+            //     setmaterialsNeeded: value.materialsNeeded
+        };
+
 
     return (
         <main>
@@ -60,16 +86,16 @@ export default function UpdateCourse({context}) {
               <div className="main--flex">
                 <div>
                   <label htmlFor="title">Course Title</label>
-                  <input id="title" name="title" type="text" value={title} onChange ={change}/>
+                  <input id="title" name="title" type="text" value={course.title} onChange ={change}/>
                   <p>By {user.firstName} {user.lastName}</p>
                   <label htmlFor="description">Course Description</label>
-                  <textarea id="description" name="description" value={description} onChange ={change}/>
+                  <textarea id="description" name="description" value={course.description} onChange ={change}/>
                 </div>
                 <div>
                   <label htmlFor="estimatedTime">Estimated Time</label>
-                  <input id="estimatedTime" name="estimatedTime" type="text" value={estimatedTime} onChange ={change}/>
+                  <input id="estimatedTime" name="estimatedTime" type="text" value={course.estimatedTime} onChange ={change}/>
                   <label htmlFor="materialsNeeded">Materials Needed</label>
-                  <textarea id="materialsNeeded" name="materialsNeeded" value={materialsNeeded} onChange ={change}/>
+                  <textarea id="materialsNeeded" name="materialsNeeded" value={course.materialsNeeded} onChange ={change}/>
                 </div>
               </div>
                 </React.Fragment>  )}/> 
@@ -84,17 +110,22 @@ export default function UpdateCourse({context}) {
     }
 //function to type when there is an event 
 function change(e){
-    const name = e.target.name;
-    const value = e.target.value;
+    const { name, value } = e.target;
 
-    if (name === 'title') {
-        setTitle(value) //grab the value to whatever 
-    } else if (name === 'description'){
-        setDescription(value); //grab the value to whatever 
-    } else if (name === 'estimatedTime'){
-        setTime(value); //grab the value to whatever 
-    } else if (name === 'materialsNeeded'){
-        setmaterialsNeeded(value);
-    }
+    setCourse(prevState => ({
+        ...prevState,
+        [name]: value,
+    }));
+  };
+
+    // if (name === 'title') {
+    //     setTitle(value) //grab the value to whatever 
+    // } else if (name === 'description'){
+    //     setDescription(value); //grab the value to whatever 
+    // } else if (name === 'estimatedTime'){
+    //     setTime(value); //grab the value to whatever 
+    // } else if (name === 'materialsNeeded'){
+    //     setmaterialsNeeded(value);
+    // }
 };
-}
+
