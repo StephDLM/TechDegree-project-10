e4baba13465ef20b9fe8ef7ad133886bdff5f7cb
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-08-12 00:00:39 -0700

    ternary operator not working in courseDetails

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -4,17 +4,14 @@ import { Link, useHistory, useParams } from "react-router-dom";
 import ReactMarkdown from 'react-markdown';
 import { Context } from '../Context';
 import { Buffer } from 'buffer';
-import authenticateUser  from '../Context';
 // import authUser from './Authenticated'
 
 export default function CourseDetail({context}) {
     const [course, setCourse] = useState(null);//initializing an array-->[variables, what you call when you wanna update courses]-
     const authUser = context.authenticatedUser;
-    // let emailAddress = authenticateUser.emailAddress
-    // let userPassword = authenticateUser.password
     let history = useHistory();
     let { id } = useParams();
-    // console.log(course);
+    console.log(course);
 
     useEffect(() => {
         function fetchCourses() {
@@ -29,31 +26,38 @@ export default function CourseDetail({context}) {
         }, []); //list variables that you want useeffect to depend on, this will just do what useEffect used for
 
         // DELETE request using fetch with error handling
-        function deleteCourse() {
-            fetch(`http://localhost:5000/api/courses/${id}`, { 
-                method: 'DELETE',
-                headers: {
-                    'Authorization':
-                      "Basic " + Buffer.from(`${authUser.emailAddress}:${authUser.password}`).toString("base64"),
-                    "Content-Type": "application/json"
-                },
-                    body: null,
-                }) 
+    function deleteCourse() {
+        fetch(`http://localhost:5000/api/courses/${id}`, { 
+            method: 'DELETE',
+            headers: {
+                'Authorization':
+                    "Basic " + Buffer.from(`${authUser.emailAddress}:${authUser.password}`).toString("base64"),
+                "Content-Type": "application/json"
+            },
+                body: null,
+            }
+            )
             .then(res => res.json())
-            .then(() => history.push("/"))
-            .catch(err => console.log(err));
-            };
+            .then(history.push("/"))
+            .catch(err => console.log(err))
+        }
 // use params from from : https://v5.reactrouter.com/web/api/Hooks/useparams
 //Rendering HTML 
-return(
+return (
         <main>
             <div className="actions--bar">
                 <div className="wrap">
-
+                { (authUser && course.user === context.course.userId) ? 
+                    <React.Fragment>
                     <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
-                    <Link className="button" to="/" onClick={ deleteCourse }>Delete Course</Link> 
+                    <button className="button" to="/" onClick={ deleteCourse }>Delete Course</button> 
                     <Link className="button button-secondary" to="/">Return to List</Link>
-            </div> 
+                    </React.Fragment>
+                    :
+                    <Link className="button button-secondary" to="/">Return to List</Link>
+
+                    }
+                </div>
             </div>
        {course !== null ?
             <div className="wrap">
